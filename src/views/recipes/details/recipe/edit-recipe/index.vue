31f1b64953323d4128f0471593855608e245793f
Karol Dembski <krldembski@gmail.com>
2023-03-05 23:31:59 +0100

    added edit-recipe components

---
+++ src/views/recipes/details/recipe/edit-recipe/index.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import CButton from "@/components/controls/button/index.vue";
+import RecipeFields from "../../../fields/recipe/index.vue";
+import CLoader from "@/components/feedback/loader/index.vue";
+
+export default {
+  name: "EditRecipe",
+  components: {
+    CButton,
+    RecipeFields,
+    CLoader,
+  },
+};
+</script>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useStore } from "vuex";
+import { useWindowSize } from "@/composables/window-size";
+import { Recipe } from "@/types/recipes/recipe";
+
+const { isMobile } = useWindowSize();
+const store = useStore();
+
+const props = defineProps<{
+  recipe: Recipe;
+}>();
+
+const emits = defineEmits<{
+  (e: "cancel"): void;
+}>();
+
+const isUpdatingRecipe = computed(() => store.state.recipe.isSubmitting);
+
+const updateRecipe = async () => {
+  await store.dispatch("recipe/update", props.recipe);
+  emits("cancel");
+};
+</script>
+
+<template src="./template.html"></template>
+<style src="./style.scss" lang="scss" scoped></style>
--- src/views/recipes/details/recipe/index.vue
+++ src/views/recipes/details/recipe/index.vue
@@ -3,10 +3,9 @@ import CButton from "@/components/controls/button/index.vue";
 import CDisplayTags from "@/components/data-display/display-tags/index.vue";
 import RecipeLoader from "./loader/index.vue";
 import InlineSvg from "vue-inline-svg";
-import RecipeFields from "../../fields/recipe/index.vue";
-import CLoader from "@/components/feedback/loader/index.vue";
 import CCard from "@/components/surfaces/card/index.vue";
 import AddToCalendarModal from "@/views/calendar/add-to-calendar-modal/index.vue";
+import EditRecipe from "./edit-recipe/index.vue";
 
 export default {
   name: "RecipeDetails",
@@ -15,10 +14,9 @@ export default {
     CDisplayTags,
     RecipeLoader,
     InlineSvg,
-    RecipeFields,
-    CLoader,
     CCard,
     AddToCalendarModal,
+    EditRecipe,
   },
 };
 </script>
@@ -39,7 +37,6 @@ const store = useStore();
 const route = useRoute();
 
 const isLoadingRecipe = computed(() => store.state.recipe.isLoading);
-const isUpdatingRecipe = computed(() => store.state.recipe.isSubmitting);
 const recipeId = computed(() => route.params.id);
 const tempRecipe: Ref<Recipe | undefined> = ref();
 const isEditing = ref(false);
@@ -60,11 +57,6 @@ onBeforeMount(() => {
 
 const loadRecipe = () => {
   return store.dispatch("recipe/load", recipeId.value);
-};
-
-const updateRecipe = async () => {
-  await store.dispatch("recipe/update", recipe.value);
-  isEditing.value = false;
 };
 
 const openCookidooLink = () => {
